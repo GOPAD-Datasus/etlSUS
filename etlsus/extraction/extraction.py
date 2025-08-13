4ca36086cbbc0e88f92be746bd0d1c993e84d57e
morsoletodev <106322393+morsoletodev@users.noreply.github.com>
2025-08-13 00:39:03 -0300

    feat: add extraction module

---
+++ etlsus/extraction/extraction.py
@@ -0,0 +1,33 @@
+import os
+from urllib.request import urlretrieve
+
+import config
+from .utils import get_yaml_urls
+from utils import check_file_exists
+
+
+def extract(input_file_url: str) -> None:
+    """
+    Main extraction function. Downloads from every url
+    inside the files key from input.yaml file.
+    """
+    raw_folder = config.RAW_DIR
+    extension = '.csv'
+
+    files, prefix = get_yaml_urls(input_file_url)
+
+    for year in files.keys():
+        source_link = files[year]
+        output_file = os.path.join(raw_folder,
+                                   f'{prefix}{year}{extension}')
+
+        if not check_file_exists(output_file):
+            try:
+                urlretrieve(source_link,
+                            output_file)
+            except Exception as e:
+                error_msg = (
+                    f'Failed to download {year}: {e}\n'
+                    f'Please verify the url in input.yaml'
+                )
+                print(f'Warning: {error_msg}')
---
+++ etlsus/extraction/utils.py
@@ -0,0 +1,38 @@
+from typing import Tuple
+
+import yaml
+
+
+def get_yaml_urls (input_file_url: str) -> Tuple[dict, str]:
+    """
+    Load information inside input.yaml
+
+    param:
+        input_file_url (str): path to the input file
+    returns:
+        tuple: Dict of urls for download,
+               prefix for file naming
+    """
+    try:
+        with open (input_file_url) as f:
+            loaded_yaml = yaml.safe_load(f)
+
+            try:
+                prefix = loaded_yaml['prefix']
+            except KeyError:
+                error_msg = (f'Missing prefix value'
+                             f' inside yaml')
+                raise RuntimeError(error_msg)
+
+            try:
+                files = loaded_yaml['files']
+            except KeyError:
+                error_msg = (f'Missing files section'
+                             f' inside yaml')
+                raise RuntimeError(error_msg)
+
+            return files, prefix
+
+    except FileNotFoundError:
+        error_msg = f'File path {input_file_url} incorrect'
+        raise RuntimeError(error_msg)
